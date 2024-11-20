516f8fc3d29592f24d2527c92b9ad1fb77f5df1b
Yotsaphon Siribannawit <yotsaphon77@gmail.com>
2024-11-20 16:58:32 +0700

    modify function

--- src/xtore/PrimeRingStruct.pyx
+++ src/xtore/PrimeRingStruct.pyx
@@ -15,9 +15,6 @@ cdef class RingLayer:
         self.layer_id = layer_id
         self.prime_number = prime_number
         self.nodes = <RingNode**>malloc(self.prime_number * sizeof(RingNode*))
-        if self.nodes is NULL:
-            print("failed")
-
         for i in range(self.prime_number):
             self.nodes[i] = NULL
 
@@ -27,53 +24,17 @@ cdef class RingLayer:
         else:
             print("failed")
 
-    def __dealloc__(self):
-        if self.nodes:
-            for i in range(self.prime_number):
-                if self.nodes[i]:
-                    del self.nodes[i]
-            free(self.nodes)
-
 cdef class PrimeRing:
     def __init__(self, int total_layers):
         self.total_layers = total_layers
         self.layers = <RingLayer**>malloc(self.total_layers * sizeof(RingLayer*))
-        if self.layers is NULL:
-            raise MemoryError("Failed to allocate memory for ring layers.")
-
-        # Initialize layers to NULL
+        self.layers[layer_id] = RingLayer(layer_id, prime_number)
         for i in range(self.total_layers):
             self.layers[i] = NULL
-
         self.total_nodes = 0
 
-    def create_layer(self, int layer_id, int prime_number):
-        if 0 <= layer_id < self.total_layers:
-            if self.layers[layer_id] is not NULL:
-                raise ValueError(f"Layer {layer_id} already exists.")
-            self.layers[layer_id] = RingLayer(layer_id, prime_number)
-        else:
-            raise ValueError("Invalid layer ID.")
+    def add_layer(self, int layer_id, int prime_number):
+        self.total_layer += 1
+        self.layers[layer_id] = RingLayer(layer_id, prime_number)
 
-    def calculate_total_nodes(self):
-        """
-        Calculate the total number of members across all layers
-        as defined by the formula in the image.
-        """
-        self.total_nodes = 0
-        cdef int i, j
-        for j in range(1, self.total_layers + 1):
-            layer_product = 1
-            for i in range(1, j + 1):
-                if self.layers[i - 1] is not NULL:
-                    layer_product *= self.layers[i - 1].prime_number
-            self.total_nodes += layer_product
-        return self.total_nodes
-
-    def __dealloc__(self):
-        # Free memory for layers
-        if self.layers:
-            for i in range(self.total_layers):
-                if self.layers[i]:
-                    del self.layers[i]
-            free(self.layers)
+    def cal_nodes(self):
