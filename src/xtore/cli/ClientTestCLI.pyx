aa532278a6502cc497a306f9112902aea7a358a1
Yotsaphon Siribannawit <yotsaphon77@gmail.com>
2025-01-22 17:29:50 +0700

    update store process

--- src/xtore/cli/ClientTestCLI.pyx
+++ src/xtore/cli/ClientTestCLI.pyx
@@ -2,6 +2,7 @@ from xtore.common.ClientHandler cimport ClientHandler
 from xtore.test.People cimport People
 from xtore.common.Buffer cimport Buffer, initBuffer, releaseBuffer, getBytes
 from xtore.BaseType cimport i32
+from xtore.instance.BasicStorage cimport BasicStorage
 
 from libc.stdlib cimport malloc
 from cpython cimport PyBytes_FromStringAndSize
@@ -17,80 +18,54 @@ def run():
 	cdef ClientTestCLI service = ClientTestCLI()
 	service.run(sys.argv[1:])
 
-cdef class ClientTestCLI :
+cdef class ClientTestCLI:
 	cdef dict config
 	cdef object parser
 	cdef object option
 	cdef ClientHandler handler
 	cdef Buffer stream
 
-    def __init__(self):
+	def __init__(self):
 		initBuffer(&self.stream, <char *> malloc(BUFFER_SIZE), BUFFER_SIZE)
 
 	def __dealloc__(self):
 		releaseBuffer(&self.stream)
 
-	cdef run(self, list argv) :
+	def run(self, list argv):
 		self.getParser(argv)
 		self.getConfig()
 		self.checkPath()
-		cdef bytes message = self.CreateMessage()
+		cdef bytes message = self.createMessage()
 		self.handler = ClientHandler(self.config["node"][0])
 		self.handler.send(message)
-	
-	cdef bytes CreateMessage(self, BasicStorage storage) :
+
+	cdef bytes createMessage(self):
 		cdef object fake = Faker()
-        cdef list peopleList = []
 		cdef People people = People()
-		cdef int i
-		cdef int n = 1
-		for i in range(n):
-			people = People()
-			people.position = -1
-			people.ID = random.randint(1_000_000_000_000, 9_999_999_999_999)
-			people.name = fake.first_name()
-			people.surname = fake.last_name()
-            people.write(&self.stream)
-			peopleList.append(people)
-            print(people)
-		for people in peopleList:
-			storage.set(people)
+		people.position = -1
+		people.ID = random.randint(1_000_000_000_000, 9_999_999_999_999)
+		people.name = fake.first_name()
+		people.surname = fake.last_name()
+		print(people)
+		people.write(&self.stream)
 		return PyBytes_FromStringAndSize(self.stream.buffer, self.stream.position)
 
-    cdef list writePeople(self, BasicStorage storage):
-		cdef list peopleList = []
-		cdef People people
-		cdef int i
-		cdef int n = 1
-		cdef object fake = Faker()
-		cdef double start = time.time()
-		for i in range(n):
-			people = People()
-			people.position = -1
-			people.ID = random.randint(1_000_000_000_000, 9_999_999_999_999)
-			people.name = fake.first_name()
-			people.surname = fake.last_name()
-			peopleList.append(people)
-            print(people)
-		for people in peopleList:
-			storage.set(people)
-		return peopleList
-
-	cdef getParser(self, list argv) :
+	cdef getParser(self, list argv):
 		self.parser = argparse.ArgumentParser(description=__help__, formatter_class=RawTextHelpFormatter)
 		self.option = self.parser.parse_args(argv)
 
-	cdef getConfig(self) :
+	cdef getConfig(self):
 		cdef str configPath = os.path.join(sys.prefix, "etc", "xtore", "XtoreNetwork.json")
-		cdef object fd
-		with open(configPath, "rt") as fd :
+		with open(configPath, "rt") as fd:
 			self.config = json.loads(fd.read())
-			fd.close()
 
-	cdef checkPath(self) :
+	cdef checkPath(self):
 		cdef str resourcePath = self.getResourcePath()
-		if not os.path.isdir(resourcePath): os.makedirs(resourcePath)
+		if not os.path.isdir(resourcePath):
+			os.makedirs(resourcePath)
 
-	cdef str getResourcePath(self) :
-		if IS_VENV: return os.path.join(sys.prefix, "var", "xtore")
-		else: return os.path.join('/', "var", "xtore")
+	cdef str getResourcePath(self):
+		if IS_VENV: 
+			return os.path.join(sys.prefix, "var", "xtore")
+		else: 
+			return os.path.join('/', "var", "xtore")
--- src/xtore/cli/StorageCLI.pyx
+++ src/xtore/cli/StorageCLI.pyx
@@ -2,10 +2,13 @@ from xtore.common.ServerHandler cimport ServerHandler
 from xtore.test.People cimport People
 from xtore.common.Buffer cimport Buffer, initBuffer, releaseBuffer
 from xtore.BaseType cimport i32, u64
+from xtore.common.StreamIOHandler cimport StreamIOHandler
+from xtore.instance.HashIterator cimport HashIterator
+from xtore.instance.BasicStorage cimport BasicStorage
+from xtore.test.PeopleHashStorage cimport PeopleHashStorage
 
 from libc.stdlib cimport malloc
 from libc.string cimport memcpy
-from cpython cimport PyBytes_FromStringAndSize
 from argparse import RawTextHelpFormatter
 import os, sys, argparse, json, traceback, random, time
 
@@ -14,18 +17,19 @@ cdef bint IS_VENV = sys.prefix != sys.base_prefix
 cdef i32 BUFFER_SIZE = 1 << 16
 
 def run():
-	cdef StorageServiceCLI service = StorageServiceCLI()
+	cdef StorageCLI service = StorageCLI()
 	service.run(sys.argv[1:])
 
-cdef class StorageServiceCLI:
-	cdef object parser
+cdef class StorageCLI:
 	cdef dict config
+	cdef object parser
+	cdef object option
 	cdef ServerHandler handler
 	cdef Buffer stream
 
 	def __init__(self):
-		initBuffer(&self.stream, <char *> malloc(length), length)
-	
+		initBuffer(&self.stream, <char *> malloc(BUFFER_SIZE), BUFFER_SIZE)
+
 	def __dealloc__(self):
 		releaseBuffer(&self.stream)
 
@@ -33,30 +37,32 @@ cdef class StorageServiceCLI:
 		self.parser = argparse.ArgumentParser(description=__help__, formatter_class=RawTextHelpFormatter)
 		self.option = self.parser.parse_args(argv)
 
-	cdef run(self, list argv):
+	def run(self, list argv):
+		print("Start server...")
 		self.getParser(argv)
-        self.checkPath()
+		self.checkPath()
 		self.getConfig()
 		self.handler = ServerHandler(self.config["node"][0])
 		self.handler.run(self.handle)
-	
-	async def handle(self, reader:object, writer:object) -> None :
-		message:bytes = await reader.read(1024)
+
+	async def handle(self, reader: object, writer: object) -> None:
+		print("Client connection received")
+		message: bytes = await reader.read(1024)
+		print("Received message:", message)
 		cdef i32 length = len(message)
 		memcpy(self.stream.buffer, <char *> message, length)
 		cdef People people = People()
 		people.readKey(0, &self.stream)
+		print("People key read:", people)
 		self.stream.position += 4
 		people.readValue(0, &self.stream)
-        self.storePeople(peopleList)
+		print("People value read:", people)
+		print(people)
+		# self.store_people([people])
 		writer.write(message)
-		await writer.drain()
-		writer.close()
-		await writer.wait_closed()
-
-	cdef StorePeople(self, list peopleList):
+		
 		cdef str resourcePath = self.getResourcePath()
-		cdef str path = f'{resourcePath}/People.Hash.bin'
+		cdef str path = f'{resourcePath}/PeopleTest.Hash.bin'
 		cdef StreamIOHandler io = StreamIOHandler(path)
 		cdef PeopleHashStorage storage = PeopleHashStorage(io)
 		cdef bint isNew = not os.path.isfile(path)
@@ -65,37 +71,53 @@ cdef class StorageServiceCLI:
 			storage.enableIterable()
 			if isNew: storage.create()
 			else: storage.readHeader(0)
+			peopleList = self.setPeople(storage, people)
+			print("People set in storage:", peopleList)
 			storedList = self.readPeople(storage, peopleList)
+			print("Stored People List:", storedList)
 			self.comparePeople(peopleList, storedList)
 			if isNew: self.iteratePeople(storage, peopleList)
 			storage.writeHeader()
 		except:
 			print(traceback.format_exc())
 		io.close()
+		
+		print('success')
+		await writer.drain()
+		writer.close()
+		await writer.wait_closed()
 
-    cdef list readPeople(self, BasicStorage storage, list peopleList):
+	cdef list setPeople(self, BasicStorage storage, People people):
+		cdef list peopleList = []
+		cdef int i
+		cdef int n = 1
+		for i in range(n):
+			peopleList.append(people)
+		for people in peopleList:
+			storage.set(people)
+		return peopleList
+
+	cdef list readPeople(self, BasicStorage storage, list peopleList):
 		cdef list storedList = []
 		cdef People stored
 		cdef double start = time.time()
 		for people in peopleList:
 			stored = storage.get(people, None)
 			storedList.append(stored)
-            print(people)
 		return storedList
 
-    cdef comparePeople(self, list referenceList, list comparingList):
+	cdef comparePeople(self, list referenceList, list comparingList):
 		cdef People reference, comparing
 		for reference, comparing in zip(referenceList, comparingList):
 			try:
-				assert(reference.ID == comparing.ID)
-				assert(reference.income == comparing.income)
-				assert(reference.name == comparing.name)
-				assert(reference.surname == comparing.surname)
+				assert reference.ID == comparing.ID
+				assert reference.name == comparing.name
+				assert reference.surname == comparing.surname
 			except Exception as error:
 				print(reference, comparing)
 				raise error
 
-    cdef iteratePeople(self, PeopleHashStorage storage, list referenceList):
+	cdef iteratePeople(self, PeopleHashStorage storage, list referenceList):
 		cdef HashIterator iterator
 		cdef People entry = People()
 		cdef People comparing
@@ -116,9 +138,9 @@ cdef class StorageServiceCLI:
 			while iterator.getNext(entry):
 				comparing = referenceList[i]
 				try:
-					assert(entry.ID == comparing.ID)
-					assert(entry.name == comparing.name)
-					assert(entry.surname == comparing.surname)
+					assert entry.ID == comparing.ID
+					assert entry.name == comparing.name
+					assert entry.surname == comparing.surname
 				except Exception as error:
 					print(entry, comparing)
 					raise error
@@ -126,17 +148,18 @@ cdef class StorageServiceCLI:
 			elapsed = time.time() - start
 			print(f'>>> People Data of {n} are checked in {elapsed:.3}s')
 
-    cdef checkPath(self):
+	cdef checkPath(self):
 		cdef str resourcePath = self.getResourcePath()
-		if not os.path.isdir(resourcePath): os.makedirs(resourcePath)
+		if not os.path.isdir(resourcePath):
+			os.makedirs(resourcePath)
 
 	cdef str getResourcePath(self):
-		if IS_VENV: return f'{sys.prefix}/var/xtore'
-		else: return '/var/xtore'
+		if IS_VENV:
+			return f'{sys.prefix}/var/xtore'
+		else:
+			return '/var/xtore'
 
-	cdef getConfig(self) :
+	cdef getConfig(self):
 		cdef str configPath = os.path.join(sys.prefix, "etc", "xtore", "XtoreNetwork.json")
-		cdef object fd
-		with open(configPath, "rt") as fd :
+		with open(configPath, "rt") as fd:
 			self.config = json.loads(fd.read())
-			fd.close()
