ba6ea353c4e7c1d0ceb83b87c823b971d599d8a5
Yotsaphon Siribannawit <yotsaphon77@gmail.com>
2025-02-17 23:39:24 +0700

    modify and test prime ring

--- src/xtore/service/PrimeRingP.pxd
+++ src/xtore/service/PrimeRingP.pxd
@@ -11,4 +11,25 @@ cdef class PrimeRingP:
 	cdef u64 getIndex(self, u64 key, i32 layer)
 	cdef insertNode(self, dict info)
 	cdef insertLayer(self)
+	
+"""
+cdef class PrimeRingP:
+	cdef list layers
+	cdef list primeNumbers
+	cdef i32 numLayers
+	cdef list nodes
 
+	cdef dict createNode(self, i32 nodeID)
+	cdef i32 getNode(self, u64 key)
+	cdef i32 getIndex(self, u64 key, i32 layer)
+	cdef insertNode(self, i32 nodeID, i32 layer)
+	cdef insertLayer(self)
+"""
+
+cdef class TreeNode:
+	cdef i32 nodeID
+	cdef i32 prime
+	cdef i32 port
+	cdef list child
+
+	cdef addChild(self, TreeNode child)
--- src/xtore/service/PrimeRingP.pyx
+++ src/xtore/service/PrimeRingP.pyx
@@ -1,5 +1,5 @@
 from xtore.BaseType cimport i32, u64
-import json
+
 
 cdef class PrimeRingP:
 	def __init__(self, list primeNumbers):
@@ -33,3 +33,97 @@ cdef class PrimeRingP:
 	cdef insertLayer(self):
 		self.layers.append({})
 		self.numLayers += 1
+
+
+"""
+cdef class PrimeRingP:
+	def __init__(self, list primeNumbers):
+		self.numLayers = 1
+		self.primeNumbers = primeNumbers
+		self.layers = [{}]
+		self.nodes = []
+
+	cdef u64 getNode(self, u64 key):
+		cdef i32 layer = 0
+		cdef i32 pathIndex
+		cdef dict node
+
+		while layer < self.numLayers:
+			pathIndex = key % self.primeNumbers[layer]
+
+			if pathIndex in self.layers[layer]:
+				node = self.layers[layer][pathIndex]
+
+				if node["splitNodes"]:
+					layer += 1
+					continue
+
+				return node["id"]
+
+			return -1
+
+		return -1
+
+	cdef u64 getIndex(self, u64 key, i32 layer):
+		return key % self.primeNumbers[layer]
+
+	cdef insertNode(self, i32 nodeID, i32 layer):
+		while layer >= self.numLayers:
+			self.insertLayer()
+
+		pathIndex = nodeID % self.primeNumbers[layer]
+
+		if pathIndex in self.layers[layer]:
+			parentNode = self.layers[layer][pathIndex]
+
+			if layer + 1 >= self.numLayers:
+				self.insertLayer()
+
+			parentNode["splitNodes"][nodeID] = self.createNode(nodeID)
+
+		self.layers[layer][pathIndex] = self.createNode(nodeID)
+		self.nodes.append(nodeID)
+
+	cdef insertLayer(self):
+		if self.numLayers >= len(self.primeNumbers):
+			return
+
+		self.layers.append({})
+		self.numLayers += 1
+"""
+
+cdef class TreeNode:
+	def __init__(self, i32 nodeID, i32 layer, i32 prime):
+		self.nodeID = nodeID
+		self.layer = layer
+		self.prime = prime
+		self.child = []
+	
+	cdef addChild(self, TreeNode child):
+		self.child.append(child)
+
+cdef list genNode(int parentPrime, int nextLayer):
+	cdef list child = []
+	for i in range(nextLayer):
+		child.append(f"Node i")
+	return child
+
+cdef TreeNode initPrimeRing(list primeNumbers):
+	cdef int layers = len(primeNumbers)
+	cdef TreeNode root = TreeNode("Root", 1, 1)
+	cdef list currentLayer = [root]
+
+	for layer in range(0, layers):
+		nextLayerPrime = primeNumbers[layer]
+		nextLayer = []
+
+		for parentNode in currentLayer:
+			child = genNode(parentNode.prime, nextLayerPrime)
+			for i, nodeID in enumerate(child):
+				childNode = TreeNode(nodeID, layer + 1, primeNumbers[layer])
+				parentNode.addChild(childNode)
+				nextLayer.append(childNode)
+
+		currentLayer = nextLayer
+
+	return root
